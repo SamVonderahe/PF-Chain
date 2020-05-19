dc38d4df5367a6bb931e7cc27511b58140088398
SamVonderahe <samvonderahe@gmail.com>
2020-05-19 17:06:07 -0400

    Rename

---
+++ pitch.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+artist=$(echo $1 | sed -r 's/ /%20/g')
+rm final
+touch final
+wget \
+\
+\
+\
+\
+\
+-O $artist.html https://pitchfork.com/search/?query=$artist
+grep -oP '\/reviews\/albums\/[^(\">)]*' $artist.html > links.html
+while read -r line; do
+    if [ ${#line} -gt 16 ]
+      then
+        echo $line
+      fi
+done < links.html > albums.html
+
+
+declare -i num=1
+while read -r line; do
+  wget -O review$num.html pitchfork.com/$line;
+  num=$((num + 1))
+  sleep 0.5
+done < albums.html
+
+
+grep -oP "[^\"]* \| [0-9].?[0-9]" review*.html > data
+grep -oP 'release_year\":[0-9]*' review*.html >> data
+declare -i dup=0;
+for (( i=1; i<$num; i++ ))
+  do
+    while read -r line; do
+      if [[ "$line" == "review$i."* || $num -eq 2 ]]; then
+        if [[ $dup -eq 0 ]]; then
+          printf "\n" >> final
+          dup=1
+        else
+          echo $line | cut -f2- -d: | cut -f2- -d: >> final
+        fi
+      fi
+    done < data
+  dup=0
+done
+
+rm *.html
+rm data
+printf "\n$1:\n======================================\n"
+cat final
+
